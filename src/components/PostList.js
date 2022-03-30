a30fdbdef4a6ec02a2b0521eedc1d5e0ac09591e
natacha <nat_bartu@hotmail.com>
2022-03-30 12:47:01 +1000

    get data on the console for testing

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,8 +8,12 @@ class PostList extends Component {
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>PostList</div>;
   }
 }
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
 
-export default connect(null, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPosts })(PostList);
