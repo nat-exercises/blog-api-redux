2ed4be3ad28ee13cc2958fc7d7b28579380c7d0a
natacha <nat_bartu@hotmail.com>
2022-03-30 10:57:44 +1000

    fetch data from api by using redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,12 @@
+import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
 export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS",
+  return function (dispatch, getState) {
+    const response = jsonPlaceHolder.get("/posts");
+
+    return {
+      type: "FETCH_POSTS",
+      payload: response,
+    };
   };
 };
